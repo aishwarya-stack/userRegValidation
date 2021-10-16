2b293984eb7a96b88b32705c1939ba6605cbcc25
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-16 13:34:04 +0530

    Add:validation of email

--- main/java/com/bridgelabz/userReg/userRegisteration.java
+++ main/java/com/bridgelabz/userReg/userRegisteration.java
@@ -17,10 +17,18 @@ public class userRegisteration {
 		String lastName = sc.nextLine();
 		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
 	}
+	public void validEmailId() {
+		System.out.println("Enter your Email ID : ");
+		String email = sc.nextLine();
+		System.out.println(
+				Pattern.matches("\"^([a][b][c])([\\\\.][a-z]+)[@][b][l][\\\\.][c][o]([\\\\.][i][n])\"", email));
+
+	}
 
 	public static void main(String[] args) {
 		userRegisteration object = new userRegisteration();
 		object.validFirstName();
 		object.validLastName();
+		object.validEmailId();
 	}
 }
